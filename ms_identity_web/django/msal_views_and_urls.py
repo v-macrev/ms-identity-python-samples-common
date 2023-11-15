c98d08f705571ae0b33dc581326d8888576f27e8
Victor 'Macrev <v0042447@ems.com.br>
2023-11-15 02:49:22 -0300

    Update msal_views_and_urls.py

--- ms_identity_web/django/msal_views_and_urls.py
+++ ms_identity_web/django/msal_views_and_urls.py
@@ -1,4 +1,9 @@
-import os, environ
+import os
+import environ
+from django.urls import path
+from django.shortcuts import redirect
+from django.urls import reverse
+import logging
 
 env = environ.Env()
 
@@ -9,13 +14,6 @@ CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, 'env_file'))
 
-try:
-    from django.urls import path
-    from django.shortcuts import redirect
-    from django.urls import reverse
-except:
-    pass
-import logging
 
 class MsalViews:
     def __init__(self, ms_identity_web):
@@ -23,6 +21,10 @@ class MsalViews:
         self.ms_identity_web = ms_identity_web
         self.prefix = self.ms_identity_web.aad_config.django.auth_endpoints.prefix + "/"
         self.endpoints = self.ms_identity_web.aad_config.django.auth_endpoints
+
+    def get_redirect_uri(self, request):
+        protocol = "http" if os.environ.get('ENVIROMENT') != "PROD" else "https"
+        return f"{protocol}://{request.get_host()}{reverse(self.endpoints.redirect)}"
 
     def url_patterns(self):
         return [
@@ -35,27 +37,28 @@ class MsalViews:
 
     def sign_in(self, request):
         self.logger.debug(f"{self.prefix}{self.endpoints.sign_in}: request received. will redirect browser to login")
-        auth_url = self.ms_identity_web.get_auth_url(redirect_uri=request.build_absolute_uri(reverse(self.endpoints.redirect)))
+        auth_url = self.ms_identity_web.get_auth_url(redirect_uri=self.get_redirect_uri(request))
         return redirect(auth_url)
 
     def edit_profile(self, request):
         self.logger.debug(f"{self.prefix}{self.endpoints.edit_profile}: request received. will redirect browser to edit profile")
         auth_url = self.ms_identity_web.get_auth_url(
-                redirect_uri=request.build_absolute_uri(reverse(self.endpoints.redirect)),
+                redirect_uri=self.get_redirect_uri(request),
                 b2c_policy=self.ms_identity_web.aad_config.b2c.profile)
         return redirect(auth_url)
 
     def aad_redirect(self, request):
         self.logger.debug(f"{self.prefix}{self.endpoints.redirect}: request received. will process params")
         return self.ms_identity_web.process_auth_redirect(
-            redirect_uri=request.build_absolute_uri(reverse(self.endpoints.redirect)),
-            afterwards_go_to_url=reverse(os.environ.get('REDIRECT_URL_MS')))
+            redirect_uri=self.get_redirect_uri(request),
+            afterwards_go_to_url=reverse(os.environ.get('REDIRECT_URL_MS'))
+        )
 
     def sign_out(self, request):
         self.logger.debug(f"{self.prefix}{self.endpoints.sign_out}: signing out username: {request.identity_context_data.username}")
-        return self.ms_identity_web.sign_out(request.build_absolute_uri(reverse(self.endpoints.post_sign_out)))    # send the user to Azure AD logout endpoint
+        return self.ms_identity_web.sign_out(request.build_absolute_uri(reverse(self.endpoints.post_sign_out)))
 
     def post_sign_out(self, request):
         self.logger.debug(f"{self.prefix}{self.endpoints.post_sign_out}: clearing session for username: {request.identity_context_data.username}")
-        self.ms_identity_web.remove_user(request.identity_context_data.username)  # remove user auth from session on successful logout
-        return redirect(reverse(os.environ.get('REDIRECT_URL_MS')))                   # take us back to the home page
+        self.ms_identity_web.remove_user(request.identity_context_data.username)
+        return redirect(reverse(os.environ.get('REDIRECT_URL_MS')))
